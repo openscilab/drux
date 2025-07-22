1e3e3ddc78b3c9396e29fd71517c3263684ce4ff
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-07-23 00:43:19 +0330

    fix : autopep8

--- tests/test_higuchi.py
+++ tests/test_higuchi.py
@@ -34,7 +34,7 @@ def test_invalid_parameters():
         HiguchiModel(D=D, c0=0.5, cs=1).simulate(duration=SIM_DURATION, time_step=SIM_TIME_STEP)
 
 
-def test_higuchi_simulation(): # Reference: https://www.sciencedirect.com/science/article/abs/pii/S0022354915333037
+def test_higuchi_simulation():  # Reference: https://www.sciencedirect.com/science/article/abs/pii/S0022354915333037
     model = HiguchiModel(D=D, c0=C0, cs=CS)
     profile = model.simulate(duration=SIM_DURATION, time_step=SIM_TIME_STEP)
     actual_release = [sqrt(D * t * (2 * C0 - CS) * CS) for t in range(0, 1001, 10)]
@@ -46,10 +46,10 @@ def test_higuchi_simulation_errors():
 
     with raises(ValueError, match="Duration and time step must be positive values"):
         model.simulate(duration=-100, time_step=10)
-    
+
     with raises(ValueError, match="Duration and time step must be positive values"):
         model.simulate(duration=100, time_step=-10)
-    
+
     with raises(ValueError, match="Time step cannot be greater than duration"):
         model.simulate(duration=10, time_step=20)
 
@@ -71,24 +71,27 @@ def test_higuchi_plot_error():
 
     with raises(ValueError, match=escape("No simulation data available. Run simulate() first.")):
         model.plot()
-    
+
     model.time_points = [0]  # manually set time points to simulate error (TODO: it will be caught with prior errors)
-    model.release_profile = [0.0]  # manually set a too short profile to simulate error (TODO: it will be caught with prior errors)
+    # manually set a too short profile to simulate error (TODO: it will be caught with prior errors)
+    model.release_profile = [0.0]
     with raises(ValueError, match="Release profile is too short to calculate release rate."):
         model.plot()
-    
+
     model.simulate(duration=SIM_DURATION, time_step=SIM_TIME_STEP)
     with mock.patch.dict('sys.modules', {'matplotlib': None}):
         with raises(ImportError, match="Matplotlib is required for plotting but not installed."):
             model.plot()
 
 
-def test_higuchi_release_rate(): # Reference: https://www.wolframalpha.com/input?i=get+the+derivative+of+sqrt%28D*C_s*%282*C_0-C_s%29*t%29+with+respect+to+t
+def test_higuchi_release_rate():  # Reference: https://www.wolframalpha.com/input?i=get+the+derivative+of+sqrt%28D*C_s*%282*C_0-C_s%29*t%29+with+respect+to+t
     model = HiguchiModel(D=D, c0=C0, cs=CS)
     model.simulate(duration=SIM_DURATION, time_step=SIM_TIME_STEP)
     rate = model.get_release_rate()
-    actual_rate = [sqrt(D * t * (2 * C0 - CS) * CS) / (2 * t) for t in range(1, 1001, 10)] # not defined at t=0, set to 0
-    assert all(isclose(r, a, rtol=1e-2) for r, a in zip(rate[10:], actual_rate[10:])) # skip first point to avoid near zero division issues
+    actual_rate = [sqrt(D * t * (2 * C0 - CS) * CS) / (2 * t)
+                   for t in range(1, 1001, 10)]  # not defined at t=0, set to 0
+    # skip first point to avoid near zero division issues
+    assert all(isclose(r, a, rtol=1e-2) for r, a in zip(rate[10:], actual_rate[10:]))
 
 
 def test_higuchi_release_rate_error():
@@ -98,12 +101,13 @@ def test_higuchi_release_rate_error():
         model.get_release_rate()
 
     model.time_points = [0]  # manually set time points to simulate error (TODO: it will be caught with prior errors)
-    model.release_profile = [0.0]  # manually set a too short profile to simulate error (TODO: it will be caught with prior errors)
+    # manually set a too short profile to simulate error (TODO: it will be caught with prior errors)
+    model.release_profile = [0.0]
     with raises(ValueError, match="Release profile is too short to calculate release rate."):
         model.get_release_rate()
 
 
-def test_higuchi_time_for_release(): # Reference: https://www.wolframalpha.com/input?i=solve+for+t+in+sqrt%2810%5E%28-6%29*0.5*%282*1.5-0.5%29*t%29+%3D+0.5*sqrt%2810%5E%28-6%29*0.5*%282*1.5-0.5%29*1000%29
+def test_higuchi_time_for_release():  # Reference: https://www.wolframalpha.com/input?i=solve+for+t+in+sqrt%2810%5E%28-6%29*0.5*%282*1.5-0.5%29*t%29+%3D+0.5*sqrt%2810%5E%28-6%29*0.5*%282*1.5-0.5%29*1000%29
     model = HiguchiModel(D=D, c0=C0, cs=CS)
     model.simulate(duration=SIM_DURATION, time_step=SIM_TIME_STEP)
     tx = model.time_for_release(0.5 * model.release_profile[-1])
@@ -119,9 +123,9 @@ def test_higuchi_time_for_release_error():
 
     with raises(ValueError, match="Target release must be between 0 and 1."):
         model.time_for_release(-0.1)
-    
+
     with raises(ValueError, match="Target release must be between 0 and 1."):
         model.time_for_release(2.0)
-    
+
     with raises(ValueError, match="Target release exceeds maximum release of the simulated duration."):
         model.time_for_release(min(model.release_profile[-1] + 0.1, 1))
