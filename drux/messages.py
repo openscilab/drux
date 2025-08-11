6b6de8719c6b0bf9763c396a522232a4453ed06e
Alireza Zolanvari <36195808+alirezazolanvari@users.noreply.github.com>
2025-08-11 11:07:11 +0200

    Bugfix release range (#20)
    * add higuchi plot
    
    * first draft of readme
    
    * add references
    
    * minor edits
    
    * minor fix
    
    * remove check if release value is more than 1
    
    * add release range bugfix

--- drux/messages.py
+++ drux/messages.py
@@ -8,7 +8,7 @@ ERROR_NO_SIMULATION_DATA = "No simulation data available. Run simulate() first."
 ERROR_RELEASE_PROFILE_TOO_SHORT = (
     "Release profile is too short to calculate release rate."
 )
-ERROR_TARGET_RELEASE_RANGE = "Target release must be between 0 and 1."
+ERROR_TARGET_RELEASE_RANGE = "Target release must be non-negative."
 ERROR_TARGET_RELEASE_EXCEEDS_MAX = (
     "Target release exceeds maximum release of the simulated duration."
 )
