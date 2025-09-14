95b0643aaee998fbc56eaae915cbc23b21c567c1
Alireza Zolanvari <36195808+alirezazolanvari@users.noreply.github.com>
2025-09-14 18:30:59 +0200

    First order model (#23)
    * add higuchi plot
    
    * first draft of readme
    
    * add references
    
    * minor edits
    
    * minor fix
    
    * first draft
    
    * add tests
    
    * fix docstrings
    
    * reformatted by black
    
    * minor edit
    
    * update CHANGELOG
    
    * fix typo
    
    * add first-order ref

--- drux/__init__.py
+++ drux/__init__.py
@@ -4,5 +4,6 @@
 from .params import DRUX_VERSION
 from .higuchi import HiguchiModel, HiguchiParameters
 from .zero_order import ZeroOrderModel, ZeroOrderParameters
+from .first_order import FirstOrderModel, FirstOrderParameters
 
 __version__ = DRUX_VERSION
