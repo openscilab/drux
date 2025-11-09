ae6c5d113366a94dc6590b9eedb1de77bc37307f
Alireza Zolanvari <36195808+alirezazolanvari@users.noreply.github.com>
2025-11-09 21:46:05 +0100

    Weibull (#32)
    * add weibull messages
    
    * add weibull model
    
    * add weibull test
    
    * add weibull to readme
    
    * add weibull
    
    * add weibull
    
    * reformatted by black
    
    * minor fix
    
    * resolve minor error in higuchi applications
    
    * minor edit
    
    * accept 0 total value weibull
    
    * minor edit in image addr

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- Weibull model
 ### Changed
 - `Python 3.14` added to `test.yml`
 ## [0.2] - 2025-09-27
--- drux/__init__.py
+++ drux/__init__.py
@@ -5,5 +5,6 @@ from .params import DRUX_VERSION
 from .higuchi import HiguchiModel, HiguchiParameters
 from .zero_order import ZeroOrderModel, ZeroOrderParameters
 from .first_order import FirstOrderModel, FirstOrderParameters
+from .weibull import WeibullModel, WeibullParameters
 
 __version__ = DRUX_VERSION
