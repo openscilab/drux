36398fe6cb2ae31cb837bbf35290ea8034d16b4d
alirezazolanvari <alireza.zolanvari93@gmail.com>
2026-02-24 12:21:51 +0100

    update with central model registry

--- drux/weibull.py
+++ drux/weibull.py
@@ -2,35 +2,13 @@
 """Drux Weibull model implementation."""
 
 from .base_model import DrugReleaseModel
-from .messages import (
-    ERROR_WEIBULL_SCALE_PARAMETER,
-    ERROR_RELEASABLE_AMOUNT,
-    ERROR_WEIBULL_SHAPE_PARAMETER,
-)
-from dataclasses import dataclass
-from math import exp
-
-
-@dataclass
-class WeibullParameters:
-    """
-    Parameters for the Weibull model based on physical formulation.
-
-    Attributes:
-        M (float): entire releasable amount of drug (normally M > 0) (mg)
-        a (float): scale factor
-        b (float): shape factor
-    """
-
-    M: float
-    a: float
-    b: float
+from .utils import create_parameters_dataclass
 
 
 class WeibullModel(DrugReleaseModel):
     """Simulator for the Weibull drug release model using analytical expressions based on concentration conditions."""
 
-    def __init__(self, M: float, a: float, b: float) -> None:
+    def __init__(self, a: float, b: float, M: float = 1) -> None:
         """
         Initialize the Weibull model with the given parameters.
 
@@ -39,34 +17,11 @@ class WeibullModel(DrugReleaseModel):
         :param b: shape factor
         """
         super().__init__()
-        self._parameters = WeibullParameters(M=M, a=a, b=b)
+        self._model_name = "weibull"
+        _params_class = create_parameters_dataclass(self._model_name)
+        self._parameters = _params_class(M=M, a=a, b=b)
         self._plot_parameters["label"] = "Weibull Model"
 
     def __repr__(self):
         """Return a string representation of the Weibull model."""
         return f"drux.WeibullModel(M={self._parameters.M}, a={self._parameters.a}, b={self._parameters.b})"
-
-    def _model_function(self, t: float) -> float:
-        """
-        Calculate the drug release at time t using the Weibull model.
-
-        Formula:
-        - General case: Mt = M * (1 - exp(-a*t ** b))
-        :param t: time (s)
-        """
-        M = self._parameters.M
-        a = self._parameters.a
-        b = self._parameters.b
-
-        Mt = M * (1 - exp(-a * t**b))
-
-        return Mt
-
-    def _validate_parameters(self) -> None:
-        """Validate the parameters of the Weibull model."""
-        if self._parameters.M < 0:
-            raise ValueError(ERROR_RELEASABLE_AMOUNT)
-        if self._parameters.a <= 0:
-            raise ValueError(ERROR_WEIBULL_SCALE_PARAMETER)
-        if self._parameters.b <= 0:
-            raise ValueError(ERROR_WEIBULL_SHAPE_PARAMETER)
