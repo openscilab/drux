f12b2338862027b0dfeeae6ba847b797d3d99132
alirezazolanvari <alireza.zolanvari93@gmail.com>
2026-05-14 13:23:02 +0200

    use create_model_class

--- drux/weibull.py
+++ drux/weibull.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
 """Drux Weibull model implementation."""
 
-from .base_model import DrugReleaseModel
-from .utils import create_parameters_dataclass
+from .base_model import create_model_class
 
-
-class WeibullModel(DrugReleaseModel):
-    """Simulator for the Weibull drug release model using analytical expressions based on concentration conditions."""
-
-    def __init__(self, a: float, b: float, M: float = 1) -> None:
-        """
-        Initialize the Weibull model with the given parameters.
-
-        :param M: entire releasable amount of drug (normally M > 0) (mg)
-        :param a: scale factor
-        :param b: shape factor
-        """
-        super().__init__()
-        self._model_name = "weibull"
-        _params_class = create_parameters_dataclass(self._model_name)
-        self._parameters = _params_class(M=M, a=a, b=b)
-        self._plot_parameters["label"] = "Weibull Model"
-
-    def __repr__(self):
-        """Return a string representation of the Weibull model."""
-        return f"drux.WeibullModel(M={self._parameters.M}, a={self._parameters.a}, b={self._parameters.b})"
+WeibullModel = create_model_class(
+    model_name="weibull",
+    class_name="WeibullModel",
+    label="Weibull Model",
+    docstring="Simulator for the Weibull drug release model using analytical expressions based on concentration conditions.",
+)
