18dc49eaa618b74a2b14dbf248737ca8efa4fbd1
alirezazolanvari <alireza.zolanvari93@gmail.com>
2026-05-14 20:00:49 +0200

    hardcode versions here

--- drux/__init__.py
+++ drux/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """Drux modules."""
 
-from .params import DRUX_VERSION
+__version__ = "0.3"
+
 from .higuchi import HiguchiModel
 from .zero_order import ZeroOrderModel
 from .first_order import FirstOrderModel
 from .weibull import WeibullModel
 from .hopfenberg import HopfenbergModel
 
-__version__ = DRUX_VERSION
