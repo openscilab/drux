23ee1210867bf48f6bd1fe3468fa80c5244c95cb
alirezazolanvari <alireza.zolanvari93@gmail.com>
2026-05-14 13:22:46 +0200

    use create_model_class

--- drux/first_order.py
+++ drux/first_order.py
@@ -1,25 +1,11 @@
 # -*- coding: utf-8 -*-
 """Drux first-order model implementation."""
-from .base_model import DrugReleaseModel
-from .utils import create_parameters_dataclass
 
+from .base_model import create_model_class
 
-class FirstOrderModel(DrugReleaseModel):
-    """Simulator for the first-order drug release model."""
-
-    def __init__(self, k: float, M0: float = 1) -> None:
-        """
-        Initialize the first-order model with the given parameters.
-
-        :param k: first-order release rate constant (1/s)
-        :param M0: entire releasable amount of drug (the asymptotic maximum) (mg)
-        """
-        super().__init__()
-        self._model_name = "first_order"
-        _params_class = create_parameters_dataclass(self._model_name)
-        self._parameters = _params_class(k=k, M0=M0)
-        self._plot_parameters["label"] = "First-Order Model"
-
-    def __repr__(self):
-        """Return a string representation of the First-Order model."""
-        return f"drux.FirstOrderModel(k={self._parameters.k}, M0={self._parameters.M0})"
+FirstOrderModel = create_model_class(
+    model_name="first_order",
+    class_name="FirstOrderModel",
+    label="First-Order Model",
+    docstring="Simulator for the first-order drug release model.",
+)
