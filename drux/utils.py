d1394607be0ee07899d5d8924d56b1fd3ebcc16d
alirezazolanvari <alireza.zolanvari93@gmail.com>
2026-05-14 13:21:08 +0200

    split params into required and optional

--- drux/utils.py
+++ drux/utils.py
@@ -13,16 +13,20 @@ def create_parameters_dataclass(model_name: str):
     :return: Dataclass type for model parameters
     """
     config = MODELS_REGISTRY[model_name]
-    fields = []
+    required_fields = []
+    optional_fields = []
 
     for param_name, param_info in config["params"].items():
         param_type = param_info["type"]
         default_value = param_info["default"]
 
         if default_value is not None:
-            fields.append((param_name, param_type, field(default=default_value)))
+            optional_fields.append((param_name, param_type, field(default=default_value)))
         else:
-            fields.append((param_name, param_type))
+            required_fields.append((param_name, param_type))
+
+    # Required fields must come before optional fields in a dataclass
+    fields = required_fields + optional_fields
 
     class_name = f"{model_name.title().replace('_', '')}Parameters"
     return make_dataclass(class_name, fields)
